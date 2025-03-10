8bf5cc932bf82804ed58d8910e537de3726ec93b
JC-Ellis <jcellis87@gmail.com>
2025-03-10 16:52:02 +0000

    finished happy path of task 4

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,5 +1,6 @@
 const endpointsJson = require("../endpoints.json");
 const request = require("supertest");
+const jestSorted = require("jest-sorted")
 const app = require("../app");
 const data = require("../db/data/test-data");
 const seed = require("../db/seeds/seed");
@@ -58,7 +59,7 @@ describe("GET /api/articles/:article_id", () => {
       .get("/api/articles/4")
       .expect(200)
       .then(({ body }) => {
-        const article = body.articles;
+        const article = body.article;
         expect(article.article_id).toBe(4);
         expect(article.author).toBe("rogersop");
         expect(article.title).toBe("Student SUES Mitch!");
@@ -87,6 +88,31 @@ describe("GET /api/articles/:article_id", () => {
       .expect(400)
       .then(({ body }) => {
         expect(body.msg).toBe("bad request");
+      });
+  });
+});
+
+describe("GET /api/articles", () => {
+  test("200: responds with an array of all articles, with the properties of: author, title, article_id, topic, created_at, votes, article_img_url, comment_count", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then(({ body }) => {
+        const articles = body.articles;
+        // console.log(articles)
+        expect(articles).toBeSortedBy("created_at", {descending: true})
+        expect(articles.length).toBe(13);
+        expect(articles).toBeInstanceOf(Array);
+        articles.forEach((articleVal) => {
+          expect(typeof articleVal.author).toBe("string");
+          expect(typeof articleVal.title).toBe("string");
+          expect(typeof articleVal.article_id).toBe("number");
+          expect(typeof articleVal.topic).toBe("string");
+          expect(typeof articleVal.created_at).toBe("string");
+          expect(typeof articleVal.votes).toBe("number");
+          expect(typeof articleVal.article_img_url).toBe("string");
+          expect(typeof articleVal.comment_count).toBe("string");
+        });
       });
   });
 });
